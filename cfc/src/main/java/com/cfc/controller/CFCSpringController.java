063546dc422c528ad158c989110aaf1373c3097c
Jorge Delgado Mora <jorgedm@gmail.com>
2016-08-07 08:54:01 -0600

    remove unnecesary endpoints

--- cfc/src/main/java/com/cfc/controller/CFCSpringController.java
+++ cfc/src/main/java/com/cfc/controller/CFCSpringController.java
@@ -5,8 +5,6 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -21,23 +19,14 @@ import com.cfc.domain.GraphData;
 import com.cfc.domain.GraphicItem;
 import com.cfc.domain.Item;
 import com.cfc.domain.MainData;
-import com.cfc.domain.Message;
-import com.cfc.model.Historico;
 import com.cfc.model.Moneda;
-import com.cfc.model.Movimiento;
 import com.cfc.model.Pivot;
-import com.cfc.model.Saldo;
 import com.cfc.model.Sucursal;
 import com.cfc.model.Transaccion;
-import com.cfc.service.IHistoricoService;
 import com.cfc.service.IMonedaService;
-import com.cfc.service.IMovimientoService;
 import com.cfc.service.IPivotService;
-import com.cfc.service.ISaldoService;
 import com.cfc.service.ISucursalService;
 import com.cfc.service.ITransaccionService;
-
-import antlr.collections.Stack;
 
 @RestController
 @RequestMapping("/efectivo")
@@ -48,12 +37,6 @@ public class CFCSpringController {
 	private MainData mda = new MainData();
 	private int maxId = 0;
 	@Autowired
-	ISaldoService iSaldoService;
-	@Autowired
-	IHistoricoService iHistoricoService;
-	@Autowired
-	IMovimientoService iMovimientoService;
-	@Autowired
 	ISucursalService iSucursalService;
 	@Autowired
 	IPivotService iPivotService;
@@ -61,165 +44,35 @@ public class CFCSpringController {
 	IMonedaService iMonedaService;
 	@Autowired
 	ITransaccionService iTransaccionService;
-	
-	@RequestMapping("/hello/{player}")
-	public Message message(@PathVariable String player) {
 
-		Message msg = new Message(player, "Hello " + player);
-		return msg;
-	}
-
-	@ResponseBody
-	@RequestMapping(value = "/getSaldos", method = RequestMethod.GET, produces = "application/json")
-	public List<Saldo> getSaldos() {
-		List<Saldo> saldos =  iSaldoService.findAllSaldos();
-		if (saldos.isEmpty())
-			return Collections.emptyList();
-		else
-			return saldos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getSaldosByCurrency/{currency}", method = RequestMethod.GET, produces = "application/json")
-	public List<Saldo> getSaldosByCurrency(@PathVariable("currency") String currency) {
-		List<Saldo> saldos =  iSaldoService.findAllSaldosByCurrency(currency);
-		if (saldos.isEmpty())
-			return Collections.emptyList();
-		else
-			return saldos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getHistoricos", method = RequestMethod.GET, produces = "application/json")
-	public List<Historico> getHistoricos() {
-		List<Historico> historicos =  iHistoricoService.findAllHistoricos();
-		logger.debug("getHistoricos.");
-		if (historicos.isEmpty())
-			return Collections.emptyList();
-		else
-			return historicos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getHistoricosByCurrency/{currency}", method = RequestMethod.GET, produces = "application/json")
-	public List<Historico> getHistoricosByCurrency(@PathVariable("currency") String currency) {
-		List<Historico> historicos =  iHistoricoService.findAllHistoricosByCurrency(currency);
-		logger.debug("getHistoricosByCurrency.");
-		if (historicos.isEmpty())
-			return Collections.emptyList();
-		else
-			return historicos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getMovimientos", method = RequestMethod.GET, produces = "application/json")
-	public List<Movimiento> getMovimientos() {
-		List<Movimiento> movimientos =  iMovimientoService.findAllMovimientos();
-		logger.debug("getMovimientos.");
-		if (movimientos.isEmpty())
-			return Collections.emptyList();
-		else
-			return movimientos;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getMovimientosByCurrency/{currency}", method = RequestMethod.GET, produces = "application/json")
-	public List<Movimiento> getMovimientosByCurrency(@PathVariable("currency") String currency) {
-		List<Movimiento> movimientos =  iMovimientoService.findAllMovimientosByCurrency(currency);
-		logger.debug("getMovimientosByCurrency.");
-		if (movimientos.isEmpty())
-			return Collections.emptyList();
-		else
-			return movimientos;
-	}
-	
 	@ResponseBody
 	@RequestMapping(value = "/getSucursales", method = RequestMethod.GET, produces = "application/json")
 	public List<Sucursal> getSucursales() {
-		List<Sucursal> sucursales =  iSucursalService.findAllSucursales();
+		List<Sucursal> sucursales = iSucursalService.findAllSucursales();
 		logger.debug("getSucursales.");
 		if (sucursales.isEmpty())
 			return Collections.emptyList();
 		else
 			return sucursales;
 	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/getSucursalByName/{name}", method = RequestMethod.GET, produces = "application/json")
-	public Sucursal getSucursal(@PathVariable("name") String name) {
-		Sucursal sucursal =  iSucursalService.findSucursalByName(name);
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/manejo/data/colones", method = RequestMethod.GET, produces = "application/json")
-	public Sucursal getManejoDataColones(@PathVariable("name") String name) {
-		Sucursal sucursal =  iSucursalService.findSucursalByName(name);
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/manejo/grafico/colones", method = RequestMethod.GET, produces = "application/json")
-	public Sucursal getManejoGraficoColones(@PathVariable("name") String name) {
-		Sucursal sucursal =  iSucursalService.findSucursalByName(name);
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/manejo/data/dolares", method = RequestMethod.GET, produces = "application/json")
-	public Sucursal getManejoDataDolares(@PathVariable("name") String name) {
-		Sucursal sucursal =  iSucursalService.findSucursalByName(name);
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/manejo/grafico/dolares", method = RequestMethod.GET, produces = "application/json")
-	public Sucursal getManejoGraficoDolares(@PathVariable("name") String name) {
-		Sucursal sucursal =  iSucursalService.findSucursalByName(name);
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	@ResponseBody
-	@RequestMapping(value = "/manejo/grafico/static", method = RequestMethod.GET, produces = "application/json")
-	public List<Object[]> getManejoGraficoDolares() {
-		List<Object[]>  sucursal =  iSaldoService.getStaticData();
-		logger.debug("getSucursal.");
-		return sucursal;
-	}
-	
-	/*@RequestMapping(method = RequestMethod.GET)
-	public String sayHello(ModelMap model) {
-		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-		return "welcome";
-	}
 
-	@RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap model) {
-		model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-		return "welcome";
-	}*/
 	@ResponseBody
-	@RequestMapping(value="/getGraphData/{branch}/{currency}", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public MainData getGraphData(@PathVariable String branch,@PathVariable String currency){
-//		variacionesEfectivo = new GraphData();
-//		comportamientoEfectivo = new  GraphDatCa();
-//		
+	@RequestMapping(value = "/getGraphData/{branch}/{currency}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public MainData getGraphData(@PathVariable String branch, @PathVariable String currency) {
+		// variacionesEfectivo = new GraphData();
+		// comportamientoEfectivo = new GraphDatCa();
+		//
 		System.err.println("currency: " + currency);
 		System.err.println("Branch: " + branch);
-		
-		List<Pivot> data = iPivotService.findByMaxID(++maxId, 1150,1);
-		if( maxId > 125)
+
+		List<Pivot> data = iPivotService.findByMaxID(++maxId, 1150, 1);
+		if (maxId > 125)
 			return mda;
 		variacionesEfectivo.getyAxisValues().clear();
 		comportamientoEfectivo.getyAxisValues().clear();
 		mda = new MainData();
-		
-		//Data for the first graph
+
+		// Data for the first graph
 		GraphicItem ocioso = new GraphicItem("Ocioso", "area");
 		GraphicItem rm = new GraphicItem("rm", "area");
 		GraphicItem menudo = new GraphicItem("Menudo", "area");
@@ -230,15 +83,15 @@ public class CFCSpringController {
 		GraphicItem lcx = new GraphicItem("Lcx", "line");
 		GraphicItem lcsx = new GraphicItem("Lcsx", "line");
 		GraphicItem ptoReorden = new GraphicItem("Punto reorden", "line");
-		
-		//Data 2nd graph		
+
+		// Data 2nd graph
 		GraphicItem lcir = new GraphicItem("Lcir", "line");
 		GraphicItem lcr = new GraphicItem("Lcr", "line");
 		GraphicItem lcsr = new GraphicItem("Lcsr", "line");
 		GraphicItem varianza = new GraphicItem("Varianza", "area");
-		
+
 		for (Pivot pivot : data) {
-			//Graph. 1
+			// Graph. 1
 			ocioso.getItemValues().add(pivot.getOcioso().toPlainString());
 			rm.getItemValues().add(pivot.getLcix().toPlainString());
 			menudo.getItemValues().add(pivot.getMenudo().toPlainString());
@@ -249,14 +102,14 @@ public class CFCSpringController {
 			lcx.getItemValues().add(pivot.getLcx().toPlainString());
 			lcsx.getItemValues().add(pivot.getLcsx().toPlainString());
 			ptoReorden.getItemValues().add(pivot.getPuntoReorden().toPlainString());
-						
-			//Graph. 2
+
+			// Graph. 2
 			varianza.getItemValues().add(pivot.getReserva().toPlainString());
 			lcsr.getItemValues().add(pivot.getLcsr().toPlainString());
 			lcr.getItemValues().add(pivot.getLcr().toPlainString());
 			lcir.getItemValues().add(pivot.getLcir().toPlainString());
 		}
-		
+
 		comportamientoEfectivo.getyAxisValues().add(ocioso);
 		comportamientoEfectivo.getyAxisValues().add(rm);
 		comportamientoEfectivo.getyAxisValues().add(menudo);
@@ -267,122 +120,113 @@ public class CFCSpringController {
 		comportamientoEfectivo.getyAxisValues().add(lcx);
 		comportamientoEfectivo.getyAxisValues().add(lcsx);
 		comportamientoEfectivo.getyAxisValues().add(ptoReorden);
-				
+
 		variacionesEfectivo.getyAxisValues().add(varianza);
 		variacionesEfectivo.getyAxisValues().add(lcsr);
 		variacionesEfectivo.getyAxisValues().add(lcr);
 		variacionesEfectivo.getyAxisValues().add(lcir);
-		
-		mda.setAutorizedBalance(data.get(data.size() - 1 ).getSaldoAutorizado());
-		mda.setCurrentBalance(data.get(data.size() - 1 ).getSaldo());
-		mda.setInsuredAmount(data.get(data.size() - 1 ).getSeguroMax());		
-		mda.setLazyAmount(data.get(data.size() - 1 ).getOcioso());
-		mda.setReorderPoint(data.get(data.size() -1 ).getPuntoReorden());
+
+		mda.setAutorizedBalance(data.get(data.size() - 1).getSaldoAutorizado());
+		mda.setCurrentBalance(data.get(data.size() - 1).getSaldo());
+		mda.setInsuredAmount(data.get(data.size() - 1).getSeguroMax());
+		mda.setLazyAmount(data.get(data.size() - 1).getOcioso());
+		mda.setReorderPoint(data.get(data.size() - 1).getPuntoReorden());
 		mda.setUtilizationRate(10);
-		
-//		variacionesEfectivo.
-		
-		mda.getCashBehaviorDetail().add(new Item("Menudo",data.get(data.size()-1).getMenudo(),data.get(data.size()-2).getMenudo()));
-		mda.getCashBehaviorDetail().add(new Item("Reserva",data.get(data.size()-1).getReserva(),data.get(data.size()-2).getReserva()));
-		mda.getCashBehaviorDetail().add(new Item("LCSX",data.get(data.size()-1).getLcsx(),data.get(data.size()-2).getLcsx()));
-		mda.getCashBehaviorDetail().add(new Item("Lcx",data.get(data.size()-1).getLcx(),data.get(data.size()-2).getLcx()));
-		mda.getCashBehaviorDetail().add(new Item("Lcix",data.get(data.size()-1).getLcix(),data.get(data.size()-2).getLcix()));
-		
-		
-		mda.getCashVariations().add(new Item("Varianza" ,data.get(data.size()-1).getReserva(),data.get(data.size()-2).getReserva()));
-		mda.getCashVariations().add(new Item("LCSr" ,data.get(data.size()-1).getLcsr(),data.get(data.size()-2).getLcsr()	));
-		mda.getCashVariations().add(new Item("LCr" ,data.get(data.size()-1).getLcr(),data.get(data.size()-2).getLcr()));
-		mda.getCashVariations().add(new Item("LClr" ,data.get(data.size()-1).getLcir(),data.get(data.size()-2).getLcir()));
-		
+
+		// variacionesEfectivo.
+
+		mda.getCashBehaviorDetail()
+				.add(new Item("Menudo", data.get(data.size() - 1).getMenudo(), data.get(data.size() - 2).getMenudo()));
+		mda.getCashBehaviorDetail().add(
+				new Item("Reserva", data.get(data.size() - 1).getReserva(), data.get(data.size() - 2).getReserva()));
+		mda.getCashBehaviorDetail()
+				.add(new Item("LCSX", data.get(data.size() - 1).getLcsx(), data.get(data.size() - 2).getLcsx()));
+		mda.getCashBehaviorDetail()
+				.add(new Item("Lcx", data.get(data.size() - 1).getLcx(), data.get(data.size() - 2).getLcx()));
+		mda.getCashBehaviorDetail()
+				.add(new Item("Lcix", data.get(data.size() - 1).getLcix(), data.get(data.size() - 2).getLcix()));
+
+		mda.getCashVariations().add(
+				new Item("Varianza", data.get(data.size() - 1).getReserva(), data.get(data.size() - 2).getReserva()));
+		mda.getCashVariations()
+				.add(new Item("LCSr", data.get(data.size() - 1).getLcsr(), data.get(data.size() - 2).getLcsr()));
+		mda.getCashVariations()
+				.add(new Item("LCr", data.get(data.size() - 1).getLcr(), data.get(data.size() - 2).getLcr()));
+		mda.getCashVariations()
+				.add(new Item("LClr", data.get(data.size() - 1).getLcir(), data.get(data.size() - 2).getLcir()));
+
 		Pivot item;
-		if(data.size() == 1){
+		if (data.size() == 1) {
 			item = data.get(0);
-			mda.getCurrentBalanceHistory().add(new Item(item.getFecha().toString(),item.getSaldo() ,BigDecimal.ZERO));
+			mda.getCurrentBalanceHistory().add(new Item(item.getFecha().toString(), item.getSaldo(), BigDecimal.ZERO));
 		}
-		mda.getCurrentBalanceHistory().add(new Item("7:00", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCurrentBalanceHistory().add(new Item("7:15", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCurrentBalanceHistory().add(new Item("7:30", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCurrentBalanceHistory().add(new Item("7:45", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		
-		
-		mda.getCashValHistory().add(new Item("7:00", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCashValHistory().add(new Item("7:15", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCashValHistory().add(new Item("7:30", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		mda.getCashValHistory().add(new Item("7:45", new BigDecimal(Math.random() * 1000),new BigDecimal(Math.random() * 100000)));
-		
+		mda.getCurrentBalanceHistory()
+				.add(new Item("7:00", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCurrentBalanceHistory()
+				.add(new Item("7:15", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCurrentBalanceHistory()
+				.add(new Item("7:30", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCurrentBalanceHistory()
+				.add(new Item("7:45", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+
+		mda.getCashValHistory()
+				.add(new Item("7:00", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCashValHistory()
+				.add(new Item("7:15", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCashValHistory()
+				.add(new Item("7:30", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+		mda.getCashValHistory()
+				.add(new Item("7:45", new BigDecimal(Math.random() * 1000), new BigDecimal(Math.random() * 100000)));
+
 		String stDate = String.valueOf(new java.util.Date().getTime());
 		System.err.println(stDate);
 		variacionesEfectivo.getxAxisValues().add(String.valueOf(Calendar.getInstance().getTime().getTime()));
 		comportamientoEfectivo.getxAxisValues().add(String.valueOf(Calendar.getInstance().getTime().getTime()));
-		
-		
-		
-		
-mda.setVarEfectivoGraph(variacionesEfectivo);
-mda.setCompEfectivoGraph( comportamientoEfectivo);
-	
+
+		mda.setVarEfectivoGraph(variacionesEfectivo);
+		mda.setCompEfectivoGraph(comportamientoEfectivo);
+
 		return mda;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getPivotTable", method = RequestMethod.GET, produces = "application/json")
 	public List<Pivot> getPivotTable() {
-		List<Pivot>  pivot =  iPivotService.findAll();
+		List<Pivot> pivot = iPivotService.findAll();
 		logger.debug("getPivotTable.");
 		return pivot;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getMonedas", method = RequestMethod.GET, produces = "application/json")
 	public List<Moneda> getMonedas() {
-		List<Moneda>  moneda =  iMonedaService.findAll();
+		List<Moneda> moneda = iMonedaService.findAll();
 		logger.debug("getMonedas.");
 		if (moneda.isEmpty())
 			return Collections.emptyList();
 		else
-		return moneda;
+			return moneda;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getTransacciones", method = RequestMethod.GET, produces = "application/json")
 	public List<Transaccion> getTransacciones() {
-		List<Transaccion>  transaccion =  iTransaccionService.findAllTransacciones();
+		List<Transaccion> transaccion = iTransaccionService.findAllTransacciones();
 		logger.debug("getTransacciones.");
 		if (transaccion.isEmpty())
 			return Collections.emptyList();
 		else
-		return transaccion;
+			return transaccion;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getDetalles", method = RequestMethod.GET, produces = "application/json")
 	public List<Detalle> getDetalles() {
-		List<Detalle>  detalle =  iPivotService.getDetalles();
+		List<Detalle> detalle = iPivotService.getDetalles();
 		logger.debug("getDetalles.");
 		if (detalle.isEmpty())
 			return Collections.emptyList();
 		else
-		return detalle;
-	}
-
-	@PostConstruct
-	public void init(){
-//		List<GraphicItem> compEfectList = new ArrayList<GraphicItem>();		
-//		compEfectList.add(new GraphicItem("Menudo", "area"));
-//		compEfectList.add(new GraphicItem("Reserva", "area"));
-//		compEfectList.add(new GraphicItem("Prestamos", "area"));
-//		compEfectList.add(new GraphicItem("LCSx", "line"));
-//		compEfectList.add(new GraphicItem("LCx", "line"));
-//		comportamientoEfectivo.setyAxisValues(compEfectList);
-		
-		
-//		List<GraphicItem> varEfectList = new ArrayList<GraphicItem>();		
-//		varEfectList.add(new GraphicItem("Menudo2", "area"));
-//		varEfectList.add(new GraphicItem("Reserva2", "area"));
-//		varEfectList.add(new GraphicItem("Prestamos2", "area"));
-//		varEfectList.add(new GraphicItem("LCSx2", "line"));
-//		varEfectList.add(new GraphicItem("LCx2", "line"));
-//		variacionesEfectivo.setyAxisValues(varEfectList);
-		
+			return detalle;
 	}
 }
